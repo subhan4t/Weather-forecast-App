f0cd2c73c3040f1e27d3042d9d9fbcf85cd741e9
subhan4t <saleemsubhan2003@gmail.com>
2024-09-14 11:34:53 +0530

    success

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Weather-forecast-App/',
   server: {
     port: 3000,  
   }
-})
+});
